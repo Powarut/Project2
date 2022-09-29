c91ad043b66fb317352f70861a1ee5c5baa1525f
LioLa <88139758+Powarut@users.noreply.github.com>
2022-09-30 02:46:48 +0700

    APK

---
+++ app/src/main/java/com/example/project2/User.java
@@ -0,0 +1,58 @@
+package com.example.project2;
+
+public class User {
+    private int id;
+    private String Firstname;
+    private String Lastname;
+    private String Email;
+    private String password;
+    private String Phone;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return Firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        Firstname = firstname;
+    }
+
+    public String getLastname() {
+        return Lastname;
+    }
+
+    public void setLastname(String lastname) {
+        Lastname = lastname;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return Phone;
+    }
+
+    public void setPhone(String phone) {
+        Phone = phone;
+    }
+}
